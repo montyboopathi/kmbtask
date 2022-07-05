d2da2e8dabcd6cc432c358414a751c385018dd87
montyboopathi <105774692+montyboopathi@users.noreply.github.com>
2022-07-05 18:48:30 +0530

    Add files via upload

---
+++ KmbTask/src/main/java/JarLibrary.java
@@ -0,0 +1,14 @@
+import java.sql.SQLException;
+
+import com.crud.CreateEmployee;
+import com.crud.DeleteEmployee;
+import com.crud.UpdateEmployee;
+
+public class JarLibrary {
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
+		//CreateEmployee.Create();
+		//UpdateEmployee.Update();
+		DeleteEmployee.Delete();
+	}
+
+}
